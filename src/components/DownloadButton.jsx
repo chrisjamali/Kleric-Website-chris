07d549aa5064a4481e7f3dce11453e47a49ed2c5
chrisjamali <jamalichristopher@gmail.com>
2022-09-16 17:15:20 -0700

    css changes

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const DownloadButton = () => {
   return (
     <div>
-      <a href='https://github.com/oslabs-beta/Kleric'>
+      <a href='https://github.com/oslabs-beta/Kleric' target ='blank_'>
       <button className='download'> Download </button>
       </a>
 
